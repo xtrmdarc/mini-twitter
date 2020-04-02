2d796214b612efe75075b7bb0f9ae9b68e231b0d
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-02 12:21:23 -0500

    Create Feed and create tweet functionalities

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,26 @@
 class TweetsController < ApplicationController
   def new
+    @tweet = Tweet.new
   end
 
   def index
+    @posts = Tweet.all
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = session[:user_id]
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      flash.now[:message] = 'Could not create the tweet'
+      render 'new'
+    end
+  end
+
+  private 
+
+  def tweet_params
+    params.require('tweet').permit(:content)
   end
 end
